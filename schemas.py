d742dd8fe6bc42b2469654b712cbc2125bedc089
Preethi Angelin <preethiangelin1@gmail.com>
2026-09-14 21:39:45 +0530

    add schemas

---
+++ schemas.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class UserResponse(BaseModel):
+    username: str
+    email: str
+    class Config():
+        orm_mode = True
+
+class PostBase(BaseModel):
+    image_url: str
+    image_url_type: str
+    caption: str
+    user_id: int
+
+# For PostResponse
+class User(BaseModel):
+    username: str
+    class Config():
+        orm_mode = True
+
+class PostResponse(BaseModel):
+    id: int
+    image_url: str
+    image_url_type: str
+    caption: str
+    timestamp: datetime
+    user: User
+    class Config():
+        orm_mode = True
+
+class UserAuth(BaseModel):
+    id: int
+    username: str
+    email: str
+    
