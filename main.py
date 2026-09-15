f413a710f0be1aa3de08fee5eec9515c98950957
Preethi Angelin <preethiangelin1@gmail.com>
2026-09-15 10:07:41 +0530

    add comment router

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import post, user
+from routers import comment, post, user
 from db import models
 from db.database import engine
 from fastapi.staticfiles import StaticFiles
@@ -10,6 +10,7 @@ app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(authentication.router)
+app.include_router(comment.router)
 
 models.Base.metadata.create_all(engine)
 
---
+++ routers/comment.py
@@ -0,0 +1,17 @@
+from schemas import CommentBase, UserAuth
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from db.database import get_db
+from db import db_comment
+from auth.oauth2 import get_current_user
+
+router = APIRouter(prefix="/comments", tags=["comment"])
+
+@router.get("/{post_id}")
+def comments(post_id: int, db: Session = Depends(get_db)):
+    return db_comment.get_all(db, post_id)
+
+@router.post("/")
+def create(request: CommentBase, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
+    return db_comment.create(db, request)
+
