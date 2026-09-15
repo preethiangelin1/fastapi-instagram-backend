e7c5599221565c45ea1bbd97adcf27f3da37e0f4
Preethi Angelin <preethiangelin1@gmail.com>
2026-09-15 16:16:49 +0530

    update to async db sessions

--- db/database.py
+++ db/database.py
@@ -1,19 +1,24 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
- 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./fastapi-instagram.db"
- 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
- 
-Base = declarative_base()
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./instagram.db"
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
--- db/db_post.py
+++ db/db_post.py
@@ -1,22 +1,76 @@
-from sqlalchemy.orm.session import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 from schemas import PostBase
 from db.models import DbPost
-import datetime
+from sqlalchemy import select
+from schemas import UserAuth
+from fastapi import HTTPException, status, Depends
+from typing import Annotated
+from db.database import get_db
 
-def create_post(db: Session, request: PostBase):
+
+async def create_post(db: AsyncSession, request: PostBase, current_user: UserAuth ):
     new_post = DbPost(
-        image_url=request.image_url,
-        image_url_type=request.image_url_type,
+        image_file=request.image_file,
         caption=request.caption,
-        timestamp=datetime.datetime.now(),
-        user_id=request.user_id
+        user_id=current_user.id
     )
 
     db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
+    await db.commit()
+    await db.refresh(new_post, attribute_names=["author", "comments"])
 
     return new_post
 
-def get_all(db: Session):
-    return db.query(DbPost).all()
+async def get_all(db: AsyncSession):
+    result = await db.execute(
+            select(DbPost).options(
+                selectinload(DbPost.comments),
+                selectinload(DbPost.author)
+            )
+        )
+    return result.scalars().all()
+
+async def get_post(id: int, db: AsyncSession):
+    result = await db.execute(
+            select(DbPost)
+                .options(
+                    selectinload(DbPost.comments),
+                    selectinload(DbPost.author)
+                )
+                .where(
+                    DbPost.id == id,
+                ),
+        )
+    existing_post = result.scalars().first()
+
+    if not existing_post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found",
+        )
+    
+    return existing_post
+
+async def delete_post(id: int, db:  Annotated[AsyncSession, Depends(get_db)], current_user: UserAuth):
+    result = await db.execute(
+        select(DbPost).where(DbPost.id == id),
+    )
+    existing_post = result.scalars().first()
+
+    if not existing_post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found",
+        )
+    
+    if existing_post.user_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized to delete this post",
+        )
+
+    await db.delete(existing_post)
+    await db.commit()
+
+        
