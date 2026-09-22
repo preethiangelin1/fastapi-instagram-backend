5f62fb0997564ca325a137c84307f5162ff7ecde
Preethi Angelin <preethiangelin1@gmail.com>
2026-09-22 05:42:49 +0530

    add s3 settings

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
     )
     database_url: str
+
+    s3_bucket_name: str
+    s3_region: str = "ap-southeast-2"
+    s3_access_key_id: SecretStr | None = None
+    s3_secret_access_key: SecretStr | None = None
+    s3_endpoint_url: str | None = None
 
     secret_key: SecretStr
     algorithm: str = "HS256"
